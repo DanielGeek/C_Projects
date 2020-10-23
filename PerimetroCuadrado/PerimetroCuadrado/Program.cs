0c334e66e7b7654ce8ec867d9a1eac4a5c0b3c80
DanielGeek <daniel30081990@gmail.com>
2020-10-23 16:54:47 -0300

    PerimetroCuadrado

---
+++ PerimetroCuadrado/PerimetroCuadrado/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetroCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado, perimetro;
+            string linea;
+            Console.Write("Ingrese el lado del cuadrado:");
+            linea = Console.ReadLine();
+            lado = int.Parse(linea);
+            perimetro = lado * 4;
+            Console.Write("El perímetro del cuadrado es:");
+            Console.Write(perimetro);
+            Console.ReadKey();
+        }
+    }
+}
