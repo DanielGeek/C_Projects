dd51bbc4c0bb203176599d5e811d6375e20d94b6
DanielGeek <daniel30081990@gmail.com>
2020-10-23 17:49:11 -0300

    EstructuraCondicionalAnidada3

---
+++ EstructuraCondicionalAnidada3/EstructuraCondicionalAnidada3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraCondicionalAnidada3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string linea;
+            Console.Write("Ingrese un valor:");
+            linea = Console.ReadLine();
+            num = int.Parse(linea);
+            if (num == 0)
+            {
+                Console.Write("Se ingresó el cero");
+            }
+            else
+            {
+                if (num > 0)
+                {
+                    Console.Write("Se ingresó un valor positivo");
+                }
+                else
+                {
+                    Console.Write("Se ingresó un valor negativo");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
