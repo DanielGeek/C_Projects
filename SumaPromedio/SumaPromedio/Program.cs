7993fd0da12e3cf5864fb8e92bc5b63a7a35b100
DanielGeek <daniel30081990@gmail.com>
2020-10-23 17:05:22 -0300

    SumaPromedio

---
+++ SumaPromedio/SumaPromedio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaPromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, num4, suma, promedio;
+            string linea;
+            Console.Write("Ingrese primer valor:");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor:");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            Console.Write("Ingrese tercer valor:");
+            linea = Console.ReadLine();
+            num3 = int.Parse(linea);
+            Console.Write("Ingrese cuarto valor:");
+            linea = Console.ReadLine();
+            num4 = int.Parse(linea);
+            suma = num1 + num2 + num3 + num4;
+            promedio = suma / 4;
+            Console.Write("La suma de los cuatro valores es:");
+            Console.WriteLine(suma);
+            Console.Write("El promedio es:");
+            Console.Write(promedio);
+            Console.ReadKey();
+        }
+    }
+}
