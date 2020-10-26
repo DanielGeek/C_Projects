6c598095285a2a7b00d149836637bad3d5c4c17d
DanielGeek <daniel30081990@gmail.com>
2020-10-26 17:18:27 -0300

    CadenaDeCaracteres3

---
+++ CadenaDeCaracteres3/CadenaDeCaracteres3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CadenaDeCaracteres3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string apellido1, apellido2;
+            Console.Write("Ingrese primer apellido:");
+            apellido1 = Console.ReadLine();
+            Console.Write("Ingrese segundo apellido:");
+            apellido2 = Console.ReadLine();
+            if (apellido1 == apellido2)
+            {
+                Console.Write("Los apellidos son iguales");
+            }
+            else
+            {
+                Console.Write("Los apellidos son distintos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
