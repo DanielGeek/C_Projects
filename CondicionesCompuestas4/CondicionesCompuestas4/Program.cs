73535e7bd620789131182b16d84f2337f502f44b
DanielGeek <daniel30081990@gmail.com>
2020-10-26 10:47:33 -0300

    CondicionesCompuestas4

---
+++ CondicionesCompuestas4/CondicionesCompuestas4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionesCompuestas4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            string linea;
+            Console.Write("Ingrese primer valor:");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor:");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            Console.Write("Ingrese tercer valor:");
+            linea = Console.ReadLine();
+            num3 = int.Parse(linea);
+            if (num1 == num2 && num1 == num3)
+            {
+                int suma = num1 + num2;
+                Console.Write("La suma del primero y segundo:");
+                Console.WriteLine(suma);
+                int producto = suma * num3;
+                Console.Write("La suma del primero y segundo multiplicado por el tercero:");
+                Console.Write(producto);
+            }
+            Console.ReadKey();
+        }
+    }
+}
