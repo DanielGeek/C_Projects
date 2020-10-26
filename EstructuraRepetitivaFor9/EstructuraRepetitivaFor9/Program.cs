8d70aaafed42e39d690b58b8732dec7393cefd0a
DanielGeek <daniel30081990@gmail.com>
2020-10-26 14:17:20 -0300

    EstructuraRepetitivaFor9

---
+++ EstructuraRepetitivaFor9/EstructuraRepetitivaFor9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EstructuraRepetitivaFor9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f, valor;
+            string linea;
+            Console.Write("Ingrese un valor entre 1 y 10:");
+            linea = Console.ReadLine();
+            valor = int.Parse(linea);
+            for (f = valor; f <= valor * 12; f += valor)
+            {
+                Console.Write(f);
+                if (f < valor * 12)
+                Console.Write("-");
+            }
+            Console.ReadKey();
+        }
+    }
+}
