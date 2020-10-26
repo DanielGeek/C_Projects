0cea2cfa694fd5307298d53a87754025965f58b3
DanielGeek <daniel30081990@gmail.com>
2020-10-26 12:28:19 -0300

    EstructuraRepetitivaWhile8

---
+++ EstructuraRepetitivaWhile8/EstructuraRepetitivaWhile8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraRepetitivaWhile8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, termino;
+            x = 1;
+            termino = 11;
+            while (x <= 25)
+            {
+                Console.Write(termino);
+                
+                x += 1;
+                if(x <= 25)
+                {
+                    Console.Write(" - ");
+                }
+                termino += 11;
+            }
+            Console.ReadKey();
+        }
+    }
+}
