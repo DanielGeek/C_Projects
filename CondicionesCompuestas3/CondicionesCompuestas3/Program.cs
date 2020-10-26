d5cbaeed5dc38e1b88b499861194f560964fd874
DanielGeek <daniel30081990@gmail.com>
2020-10-26 10:45:52 -0300

    CondicionesCompuestas3

---
+++ CondicionesCompuestas3/CondicionesCompuestas3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionesCompuestas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, año;
+            string linea;
+            Console.Write("Ingrese nro de día:");
+            linea = Console.ReadLine();
+            dia = int.Parse(linea);
+            Console.Write("Ingrese nro de mes:");
+            linea = Console.ReadLine();
+            mes = int.Parse(linea);
+            Console.Write("Ingrese nro de año:");
+            linea = Console.ReadLine();
+            año = int.Parse(linea);
+            if (mes == 12 && dia == 25)
+            {
+                Console.Write("La fecha ingresada corresponde a navidad.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
