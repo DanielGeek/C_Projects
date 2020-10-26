7261289f34378517651df94977085dc8ac80dfec
DanielGeek <daniel30081990@gmail.com>
2020-10-26 14:01:15 -0300

    EstructuraRepetitivaFor7

---
+++ EstructuraRepetitivaFor7/EstructuraRepetitivaFor7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EstructuraRepetitivaFor7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f, valor, suma;
+            string linea;
+            suma = 0;
+            for (f = 1; f <= 10; f++)
+            {
+                Console.Write("Ingrese un valor:");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (f > 5)
+                {
+                    suma += valor;
+                }
+            }
+            Console.Write("La suma de los últimos 5 valores es:");
+            Console.Write(suma);
+            Console.ReadKey();
+        }
+    }
+}
