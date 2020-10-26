7199ab029a174603ed442697df8db811082701e0
DanielGeek <daniel30081990@gmail.com>
2020-10-26 13:21:34 -0300

    EstructuraRepetitivaFor1

---
+++ EstructuraRepetitivaFor1/EstructuraRepetitivaFor1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraRepetitivaFor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f;
+            for (f = 1; f <= 100; f++)
+            {
+                Console.Write(f);
+                if (f < 100)
+                {
+                    Console.Write("-");
+                }
+                
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
