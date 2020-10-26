22f8a82bceb599576a2c566a303817948825f1f2
DanielGeek <daniel30081990@gmail.com>
2020-10-26 10:59:53 -0300

    CondicionesCompuestas5

---
+++ CondicionesCompuestas5/CondicionesCompuestas5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionesCompuestas5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            string linea;
+            Console.Write("Ingrese primer valor:");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor:");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            Console.Write("Ingrese tercer valor:");
+            linea = Console.ReadLine();
+            num3 = int.Parse(linea);
+            if (num1 < 10 && num2 < 10 && num3 < 10)
+            {
+                Console.Write("Todos los números son menores a diez");
+            }
+            Console.ReadKey();
+        }
+    }
+}
