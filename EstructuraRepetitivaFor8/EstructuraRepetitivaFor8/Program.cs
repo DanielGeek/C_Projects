e7ba0dbcad689094122389d5769fdcf9ab1fd18f
DanielGeek <daniel30081990@gmail.com>
2020-10-26 14:04:01 -0300

    EstructuraRepetitivaFor8

---
+++ EstructuraRepetitivaFor8/EstructuraRepetitivaFor8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EstructuraRepetitivaFor8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f;
+            for (f = 5; f <= 50; f += 5)
+            {
+                Console.Write(f);
+                if(f < 50)
+                    Console.Write("-");
+            }
+            Console.ReadKey();
+        }
+    }
+}
