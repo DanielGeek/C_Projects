4b41123100ff78f1710c989d2d001cf21b46d20b
DanielGeek <daniel30081990@gmail.com>
2020-10-26 11:38:44 -0300

    EstructuraRepetitivaWhile2

---
+++ EstructuraRepetitivaWhile2/EstructuraRepetitivaWhile2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraRepetitivaWhile2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x;
+            string linea;
+            Console.Write("Ingrese el valor final:");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            x = 1;
+            while (x <= n)
+            {
+                Console.Write(x);
+                x += 1;
+                if( x <= n)
+                {
+                    Console.Write(" - ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
