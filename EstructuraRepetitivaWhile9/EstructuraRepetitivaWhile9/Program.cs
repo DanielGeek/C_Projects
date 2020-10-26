afefff1cdbd24ffb1c798685ac0f31a9b81ebb91
DanielGeek <daniel30081990@gmail.com>
2020-10-26 12:31:17 -0300

    EstructuraRepetitivaWhile9

---
+++ EstructuraRepetitivaWhile9/EstructuraRepetitivaWhile9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraRepetitivaWhile9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mult8;
+            mult8 = 8;
+            while (mult8 <= 500)
+            {
+                Console.Write(mult8);
+                mult8 += 8;
+                if (mult8 <= 500)
+                {
+                    Console.Write(" - ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
