ff87e97a6cb4a1f3da502cee288e5fb70b69e163
DanielGeek <daniel30081990@gmail.com>
2020-10-27 10:04:25 -0300

    Tabla

---
+++ Tabla/Tabla/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tabla
+{
+    class TablaMultiplicar
+    {
+        public void CargarValor()
+        {
+            int valor;
+            string linea;
+            do
+            {
+                Console.Write("Ingrese un valor (-1 para finalizar):");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor != -1)
+                {
+                    Calcular(valor);
+                }
+            } while (valor != -1);
+        }
+
+        public void Calcular(int v)
+        {
+            for (int f = v; f <= v * 10; f = f + v)
+            {
+                Console.Write(f);
+                if (f < v * 10)
+                {
+                    Console.Write("-");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            TablaMultiplicar tm = new TablaMultiplicar();
+            tm.CargarValor();
+        }
+    }
+}
