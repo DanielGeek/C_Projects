8692339d4f6c000ab461a8d614adfab39c0e69a0
DanielGeek <daniel30081990@gmail.com>
2020-10-27 16:50:32 -0300

    Matriz7

---
+++ Matriz7/Matriz7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Matriz7
+{
+    class Matriz7
+    {
+        private int[,] mat;
+
+        public void Cargar()
+        {
+            Console.Write("Cuantas fila tiene la matriz:");
+            string linea;
+            linea = Console.ReadLine();
+            int filas = int.Parse(linea);
+            Console.Write("Cuantas columnas tiene la matriz:");
+            linea = Console.ReadLine();
+            int columnas = int.Parse(linea);
+            mat = new int[filas, columnas];
+            for (int f = 0; f < mat.GetLength(0); f++)
+            {
+                for (int c = 0; c < mat.GetLength(1); c++)
+                {
+                    Console.Write("Ingrese componente:");
+                    linea = Console.ReadLine();
+                    mat[f, c] = int.Parse(linea);
+                }
+            }
+        }
+
+        public void Intercambiar()
+        {
+            for (int c = 0; c < mat.GetLength(1); c++)
+            {
+                int aux = mat[0, c];
+                mat[0, c] = mat[1, c];
+                mat[1, c] = aux;
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < mat.GetLength(0); f++)
+            {
+                for (int c = 0; c < mat.GetLength(1); c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Matriz7 ma = new Matriz7();
+            ma.Cargar();
+            ma.Intercambiar();
+            ma.Imprimir();
+        }
+    }
+}
