1c093680671857a8cfa06c9da4f1c4d0a4f8e8c7
DanielGeek <daniel30081990@gmail.com>
2020-10-27 18:02:27 -0300

    PruebaConstructor3

---
+++ PruebaConstructor3/PruebaConstructor3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PruebaConstructor3
+{
+    class EmpleadoFabrica
+    {
+        readonly string nombre;
+        readonly float sueldo;
+
+        public EmpleadoFabrica()
+        {
+            Console.Write("Ingrese el nombre del empleado:");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese su sueldo:");
+            string linea = Console.ReadLine();
+            sueldo = float.Parse(linea);
+        }
+
+        public void PagaImpuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.Write("Debe abonar impuestos");
+            }
+            else
+            {
+                Console.Write("No paga impuestos");
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            EmpleadoFabrica empleado1;
+            empleado1 = new EmpleadoFabrica();
+            empleado1.PagaImpuestos();
+        }
+    }
+}
