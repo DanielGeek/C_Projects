21a2a14bf36f6e666f21347ebd46c3a6c5335f29
DanielGeek <daniel30081990@gmail.com>
2020-10-27 09:32:57 -0300

    PruebaClase52

---
+++ PruebaClase5/PruebaClase5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PruebaClase52
+{
+    class Empleado
+    {
+        string nombre;
+        float sueldo;
+
+        public void Inicializar()
+        {
+            string linea;
+            Console.Write("Ingrese el nombre del empleado:");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese su sueldo:");
+            linea = Console.ReadLine();
+            sueldo = float.Parse(linea);
+        }
+
+        public void PagaImpuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("Debe abonar impuestos");
+            }
+            else
+            {
+                Console.WriteLine("No paga impuestos");
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado();
+            empleado1.Inicializar();
+            empleado1.PagaImpuestos();
+        }
+    }
+}
+
