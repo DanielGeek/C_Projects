c7860a21ffe4b525a563a7229cc446ffc6fd6df6
DanielGeek <daniel30081990@gmail.com>
2020-10-27 13:15:53 -0300

    PruebaVector9

---
+++ PruebaVector9/PruebaVector9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PruebaVector9
+{
+    class PruebaVector9
+    {
+        private int[] elementos;
+        public void Cargar()
+        {
+            Console.Write("Ingrese tamaño del Vector:");
+            string linea;
+            linea = Console.ReadLine();
+            int tamaño = int.Parse(linea);
+            elementos = new int[tamaño];
+            for (int f = 0; f < elementos.Length; f++)
+            {
+                Console.Write("Ingrese valor a sumar:");
+                linea = Console.ReadLine();
+                elementos[f] = int.Parse(linea);
+            }
+        }
+
+        public void Sumar()
+        {
+            int sumaTotal = 0;
+            for (int f = 0; f < elementos.Length; f++)
+            {
+                sumaTotal += elementos[f];
+                Console.WriteLine(elementos[f]);
+            }
+            Console.WriteLine("Suma total " + sumaTotal);
+            Console.ReadKey();
+        }
+
+        static void Main()
+        {
+            PruebaVector9 pv = new PruebaVector9();
+            pv.Cargar();
+            pv.Sumar();
+        }
+    }
+}
