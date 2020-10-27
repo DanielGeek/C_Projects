ca6a562a2296a7e980c49e6a320501209acda163
DanielGeek <daniel30081990@gmail.com>
2020-10-27 18:04:12 -0300

    PruebaConstructor4

---
+++ PruebaConstructor4/PruebaConstructor4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace PruebaConstructor4
+{
+    class OperacionesCalculo
+    {
+        int valor1, valor2;
+
+        public OperacionesCalculo()
+        {
+            Console.Write("Ingrese primer valor:");
+            string linea = Console.ReadLine();
+            valor1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor:");
+            linea = Console.ReadLine();
+            valor2 = int.Parse(linea);
+        }
+
+        public void Sumar()
+        {
+            int suma;
+            suma = valor1 + valor2;
+            Console.WriteLine("La suma es:" + suma);
+        }
+
+        public void Restar()
+        {
+            int resta;
+            resta = valor1 - valor2;
+            Console.WriteLine("La resta es:" + resta);
+        }
+
+        public void Multiplicar()
+        {
+            int multiplicacion;
+            multiplicacion = valor1 * valor2;
+            Console.WriteLine("La multiplicación es:" + multiplicacion);
+        }
+
+        public void Dividir()
+        {
+            int division;
+            division = valor1 / valor2;
+            Console.WriteLine("La división es:" + division);
+        }
+
+        static void Main(string[] args)
+        {
+            OperacionesCalculo opera = new OperacionesCalculo();
+            opera.Sumar();
+            opera.Restar();
+            opera.Multiplicar();
+            opera.Dividir();
+            Console.ReadKey();
+        }
+    }
+}
