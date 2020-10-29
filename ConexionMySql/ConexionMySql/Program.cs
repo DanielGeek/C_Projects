65350bb9664c29847111bf7c5f9c4520c80dfd3e
DanielGeek <daniel30081990@gmail.com>
2020-10-29 18:06:52 -0300

    Clase conexion con insert

---
+++ ConexionMySql/ConexionMySql/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMySql
+{
+    class Program
+    {
+        static void Main()
+        {
+            settings_mysql configuracionBD = new settings_mysql();
+            Console.WriteLine($"Zona horaria {configuracionBD.MyZone} Host {configuracionBD.Host}  User {configuracionBD.User} Password {configuracionBD.Password} Nombre BD { configuracionBD.NombreBD}");
+            ConexionMysql conn = new ConexionMysql();
+            
+            DateTime fechaActual = DateTime.Now;
+            String query = string.Format($" Insert into  contadora(peso, fecha) values('{ 0.25 }','{fechaActual}')");
+            int respuesta = conn.Insert(query);
+            Console.WriteLine(respuesta);
+            // ejecutar al finalizar una query
+            conn.Conectar().Close();
+
+
+        }
+    }
+}
---
+++ ConexionMySql/ConexionMySql/settings_mysql.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMySql
+{
+    class settings_mysql
+    {
+        //definimos atributos constantes de la conexión
+        // Zona horaria para chile
+        private TimeZoneInfo myZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific SA Standard Time");
+        private const String host = "localhost";
+        private const String user = "root";
+        private const String password = "";
+        private const String nombreBD = "balanzadinamica";
+
+        // definimos propiedades
+        public TimeZoneInfo MyZone
+        {
+            get
+            {
+                return myZone;
+            }
+        }
+        public String Host
+        {
+            get
+            {
+                return host;
+            }
+        }
+        public String User
+        {
+            get
+            {
+                return user;
+            }
+        }
+        public String Password
+        {
+            get
+            {
+                return password;
+            }
+        }
+        public String NombreBD
+        {
+            get
+            {
+                return nombreBD;
+            }
+        }
+
+    }
+}
