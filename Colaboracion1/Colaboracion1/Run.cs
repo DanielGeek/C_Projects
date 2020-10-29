227dd2775aa3e8330239cd131cf94a5b277dedbc
DanielGeek <daniel30081990@gmail.com>
2020-10-29 10:29:12 -0300

    Clase banco / cliente

---
+++ Colaboracion1/Colaboracion1/Run.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colaboracion1
+{
+    class Run
+    {
+        static void Main()
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
