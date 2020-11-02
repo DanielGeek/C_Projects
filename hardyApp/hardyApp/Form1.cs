7ecedde0a35bd795b974525e0906e11bbc7d05da
DanielGeek <daniel30081990@gmail.com>
2020-11-02 16:33:07 -0300

    asasas

--- hardyApp/hardyApp/Form1.cs
+++ hardyApp/hardyApp/Form1.cs
@@ -14,12 +14,14 @@ namespace hardyApp
     {
         String ip = "192.168.0.52";
         private double pesoHardy = 0;
+        private double pesoHardyMaximo = 0;
         List<double> ListaPesos;
         private int contador = 0;
+        ConexionMysql conn = new ConexionMysql();
         public Form1()
         {
             InitializeComponent();
-            
+
         }
 
         private void btnPesar_Click(object sender, EventArgs e)
@@ -66,49 +68,69 @@ namespace hardyApp
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            //bool ObjPresiono = (bool)e.Argument;
             ListaPesos = new List<double>();
-            ConexionMysql conn = new ConexionMysql();
-
+            bool estado = false;
             while (!backgroundWorker1.CancellationPending)
             {
                 pesoHardy = Hardy.pesohardy(ip);
-                if (pesoHardy > 0.25)
-                {
-                    DateTime fechaActual = DateTime.Now;
-                    String query = string.Format($" Insert into  contadora(peso, fecha) values('{ pesoHardy.ToString().Replace(",", ".") }','{fechaActual}')");
-                    Console.WriteLine(query);
-                    int respuesta = conn.Insert(query);
-                    Console.WriteLine($"espuesta bd {respuesta} ");
-                    // ejecutar al finalizar una query
-                    
-                    contador++;
-                    ListaPesos.Add(pesoHardy);
-                }
-                //Console.WriteLine(pesoHardy);
-                //lblPesoHardy.Text = "Peso" + pesoHardy;
-                //Console.WriteLine(pesoHardy);
                 
-                //Object ObjPeso = pesoHardy;
+                // agrego a la lista todos los pesos del pez para luego obtener el máximo en el ProgressChanged
+                ListaPesos.Add(pesoHardy);
+                this.lblPesoHardy.Invoke(new MethodInvoker(delegate ()
+                {
+                    lblPesoHardy.Text = pesoHardy.ToString();
+                }));
+                
+                
+                if (pesoHardy < 0.1)
+                {
+                    estado = false;
+                }
+                
+                if(pesoHardy > 1 && estado == false)
+                {
+                    if(ListaPesos.Count() > 1)
+                    {
+                        double ListPesMaxUltimo = ListaPesos.Last();
+                        double listPesMaxPenultimo = ListaPesos.ElementAt(ListaPesos.Count - 2);
+                        if(listPesMaxPenultimo > ListPesMaxUltimo)
+                        {
+                            estado = true;
+                            backgroundWorker1.ReportProgress(1);    
 
-                backgroundWorker1.ReportProgress(1);
+                        }
+                       
+                    }
+                    
 
-                System.Threading.Thread.Sleep(1000);
+                }
+
 
             }
-            conn.Conectar().Close();
+            
         }
 
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            //Double ObjPeso = (double)e.UserState;
-            lblPesoHardy.Text = "Peso " + pesoHardy;
+            pesoHardyMaximo = ListaPesos.Max();
+            contador++;
+            //lblPesoHardy.Text = "Peso " + pesoHardy;
             lblContadorPez.Text = "Total piezas " + contador;
+            DateTime fechaActual = DateTime.Now;
+            String query = string.Format($" Insert into  contadora(peso, fecha) values('{ pesoHardyMaximo.ToString().Replace(",", ".") }','{fechaActual}')");
+            Console.WriteLine(query);
+            // tomar el peso máximo del pez y insertarlo
+            int respuesta = conn.Insert(query);
+            Console.WriteLine($"espuesta bd {respuesta} ");
+
             //if (e.ProgressPercentage == 1)
             //{
 
             //    lblPesoHardy.Text = "Peso Máximo = " + ListaPesos.Max();
             //}
+            ListaPesos.Clear();
+
+
         }
 
         public void TrabajoCompletado(object sender, RunWorkerCompletedEventArgs e)
@@ -116,7 +138,10 @@ namespace hardyApp
             // Si terminó el trabajo (finalización, cancelación, error), hacer lo siguiente:
             Console.WriteLine();
             Console.WriteLine("Finaliza el Trabajo de prueba");
-            lblPesoHardy.Text = "Peso Máximo = " + ListaPesos.Max();
+            // peso máximo de todos los peces que pasaron
+            lblPesoHardy.Text = "Peso Máximo = " + pesoHardyMaximo;
+            conn.Conectar().Close();
+
         }
     }
 }
