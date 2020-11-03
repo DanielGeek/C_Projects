09ddc547a69f1d28c2186fea9a0fc9b3a32d5008
DanielGeek <daniel30081990@gmail.com>
2020-11-03 15:33:12 -0300

    radio button app

---
+++ radioButtonApp/radioButtonApp/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace radioButtonApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.rBSuma = new System.Windows.Forms.RadioButton();
+            this.rBResta = new System.Windows.Forms.RadioButton();
+            this.txtB1 = new System.Windows.Forms.TextBox();
+            this.txtB2 = new System.Windows.Forms.TextBox();
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(88, 53);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "640 * 480";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 91);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "800 * 600";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(88, 128);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(77, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "1024 * 768";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(88, 177);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 3;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // rBSuma
+            // 
+            this.rBSuma.AutoSize = true;
+            this.rBSuma.Location = new System.Drawing.Point(378, 53);
+            this.rBSuma.Name = "rBSuma";
+            this.rBSuma.Size = new System.Drawing.Size(55, 17);
+            this.rBSuma.TabIndex = 4;
+            this.rBSuma.TabStop = true;
+            this.rBSuma.Text = "Sumar";
+            this.rBSuma.UseVisualStyleBackColor = true;
+            // 
+            // rBResta
+            // 
+            this.rBResta.AutoSize = true;
+            this.rBResta.Location = new System.Drawing.Point(378, 91);
+            this.rBResta.Name = "rBResta";
+            this.rBResta.Size = new System.Drawing.Size(56, 17);
+            this.rBResta.TabIndex = 5;
+            this.rBResta.TabStop = true;
+            this.rBResta.Text = "Restar";
+            this.rBResta.UseVisualStyleBackColor = true;
+            // 
+            // txtB1
+            // 
+            this.txtB1.Location = new System.Drawing.Point(243, 50);
+            this.txtB1.Name = "txtB1";
+            this.txtB1.Size = new System.Drawing.Size(100, 20);
+            this.txtB1.TabIndex = 6;
+            // 
+            // txtB2
+            // 
+            this.txtB2.Location = new System.Drawing.Point(243, 88);
+            this.txtB2.Name = "txtB2";
+            this.txtB2.Size = new System.Drawing.Size(100, 20);
+            this.txtB2.TabIndex = 7;
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(493, 65);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(75, 23);
+            this.btnOperar.TabIndex = 8;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOperar);
+            this.Controls.Add(this.txtB2);
+            this.Controls.Add(this.txtB1);
+            this.Controls.Add(this.rBResta);
+            this.Controls.Add(this.rBSuma);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.RadioButton rBSuma;
+        private System.Windows.Forms.RadioButton rBResta;
+        private System.Windows.Forms.TextBox txtB1;
+        private System.Windows.Forms.TextBox txtB2;
+        private System.Windows.Forms.Button btnOperar;
+    }
+}
+
---
+++ radioButtonApp/radioButtonApp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radioButtonApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                Width = 640;
+                Height = 480;
+            }
+            else
+            {
+                if (radioButton2.Checked == true)
+                {
+                    Width = 800;
+                    Height = 600;
+                }
+                else
+                {
+                    if (radioButton3.Checked == true)
+                    {
+                        Width = 1024;
+                        Height = 768;
+                    }
+                }
+            }
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(txtB1.Text);
+            int valor2 = int.Parse(txtB2.Text);
+            if (rBSuma.Checked == true)
+            {
+                int suma = valor1 + valor2;
+                Text = suma.ToString();
+            }
+            else
+            {
+                if (rBResta.Checked == true)
+                {
+                    int resta = valor1 - valor2;
+                    Text = resta.ToString();
+                }
+            }
+        }
+    }
+}
