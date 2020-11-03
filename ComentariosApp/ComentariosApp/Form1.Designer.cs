d591ec1083f883e54caab84956e956c12df3b38d
DanielGeek <daniel30081990@gmail.com>
2020-11-03 13:44:43 -0300

    asasasa

---
+++ ComentariosApp/ComentariosApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ComentariosApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtBTitulo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBComentarios = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(80, 60);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(35, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Título";
+            // 
+            // txtBTitulo
+            // 
+            this.txtBTitulo.Location = new System.Drawing.Point(153, 60);
+            this.txtBTitulo.Name = "txtBTitulo";
+            this.txtBTitulo.Size = new System.Drawing.Size(322, 20);
+            this.txtBTitulo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Comentarios";
+            // 
+            // txtBComentarios
+            // 
+            this.txtBComentarios.Location = new System.Drawing.Point(153, 118);
+            this.txtBComentarios.Multiline = true;
+            this.txtBComentarios.Name = "txtBComentarios";
+            this.txtBComentarios.Size = new System.Drawing.Size(322, 220);
+            this.txtBComentarios.TabIndex = 3;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(400, 364);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 4;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtBComentarios);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBTitulo);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtBTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBComentarios;
+        private System.Windows.Forms.Button btnConfirmar;
+    }
+}
+
