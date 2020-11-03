c42a7a516a7ff23337c150d609c0aa26e5363c29
DanielGeek <daniel30081990@gmail.com>
2020-11-03 11:08:20 -0300

    Herencia2

---
+++ Herencia2/Herencia2/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herencia2
+{
+    public class Persona
+    {
+        protected string nombre;
+        protected int edad;
+
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+
+        public int Edad
+        {
+            set
+            {
+                edad = value;
+            }
+            get
+            {
+                return edad;
+            }
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Nombre:" + Nombre);
+            Console.WriteLine("Edad:" + Edad);
+        }
+    }
+}
--- Herencia2/Herencia2/Program.cs
+++ Herencia2/Herencia2/Program.cs
@@ -6,7 +6,20 @@ namespace Herencia2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona persona1 = new Persona();
+            persona1.Nombre = "Juan";
+            persona1.Edad = 25;
+            Console.WriteLine("Los datos de la persona son:");
+            persona1.Imprimir();
+
+            Empleado empleado1 = new Empleado();
+            empleado1.Nombre = "Ana";
+            empleado1.Edad = 42;
+            empleado1.Sueldo = 2524;
+            Console.WriteLine("Los datos del empleado son:");
+            empleado1.Imprimir();
+
+            Console.ReadKey();
         }
     }
 }
